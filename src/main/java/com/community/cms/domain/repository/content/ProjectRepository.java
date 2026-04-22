4515fea6b782fe4ba70122762dcbf3d4f36baea4
Vasickin <vasickin@mail.ru>
2026-04-22 16:40:50 +0300

    Расширен ProjectRepository для поддержки Specification

--- src/main/java/com/community/cms/domain/repository/content/ProjectRepository.java
+++ src/main/java/com/community/cms/domain/repository/content/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -29,7 +30,7 @@ import java.util.stream.Collectors;
  * @see org.springframework.data.jpa.repository.JpaRepository
  */
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
 
     // ================== ОСНОВНЫЕ МЕТОДЫ ПОИСКА ==================
 
